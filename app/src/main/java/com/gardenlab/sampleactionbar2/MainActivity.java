fd10fad3070731e4f0ea5662ae29ed275b21a972
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-29 16:05:29 +0900

    Add EditText on Action Bar

--- app/src/main/java/com/gardenlab/sampleactionbar2/MainActivity.java
+++ app/src/main/java/com/gardenlab/sampleactionbar2/MainActivity.java
@@ -1,8 +1,16 @@
 package com.gardenlab.sampleactionbar2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        View v = menu.findItem(R.id.menu_search).getActionView();
+
+        if(v != null) {
+            EditText editText = v.findViewById(R.id.editText);
+
+            if(editText !=null) {
+                editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                    @Override
+                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                        Toast.makeText(getApplicationContext(),"입력됨", Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
